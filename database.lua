975761bff4595f66d03932111cd83b216fbb16aa
N6REJ <programming@hallhome.us>
2021-11-10 18:27:22 -0600

    Added missing vendor reagents

--- database.lua
+++ database.lua
@@ -12,22 +12,31 @@ db.desc = "Mining reagents for shadowlands"
 
 -- Filter info
 db.Filters = {
-	["Ore"] = {
-		uiName = "Shadowlands Mining",
-		uiDesc = "Ore found in Shadowlands",
-		title = "Ore",
-		items = {
-			-- ID  = true,		--Item name
-			[171828] = true, -- Laestrite Ore
-			[171829] = true, -- Solenium Ore
-			[171830] = true, -- Oxxein Ore
-			[171831] = true, -- Phaedrum Ore
-			[171832] = true, -- Sinvyr Ore
-			[171833] = true, -- Elethium Ore
-			[171840] = true, -- Porous Stone
-			[171841] = true -- Shaded Stone
-		}
-	}
+    ["Ore"] = {
+        uiName = "Shadowlands Mining",
+        uiDesc = "Ore found in Shadowlands",
+        title = "Ore",
+        items = {
+            -- ID  = true,		--Item name
+            [171828] = true, -- Laestrite Ore
+            [171829] = true, -- Solenium Ore
+            [171830] = true, -- Oxxein Ore
+            [171831] = true, -- Phaedrum Ore
+            [171832] = true, -- Sinvyr Ore
+            [171833] = true, -- Elethium Ore
+            [171840] = true, -- Porous Stone
+            [171841] = true -- Shaded Stone
+        },
+    },
+    ["Vendor_Reagents"] = {
+        uiName = "Shadowlands Vendor Reagents",
+        uiDesc = "Reagents from vendors used in Shadowlands",
+        title = "Vendor Reagents",
+        items = {
+            -- ID    = true,    --Item name
+            [177061] = true, -- Twilight Bark
+        }
+    },
 }
 
 -- now that db is populated lets pass it on.
