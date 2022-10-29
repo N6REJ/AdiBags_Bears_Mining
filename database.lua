729dc58d4d5206f6feccf9c41d9959e43dec2eac
Bear <programming@hallhome.us>
2022-10-29 07:37:02 -0500

    typo

--- database.lua
+++ database.lua
@@ -6,7 +6,7 @@ local addonName, addonTable = ...;
 
 -- Get locals namse
 local L = addonTable.locales;
-local Expansion = L["All"]
+local Expansion = L["Shadowlands"]
 
 -- Create addon table
 local db = {}
